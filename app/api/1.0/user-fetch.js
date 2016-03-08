fd7b1008e01ebd55b70f2879675903ed257339bc
Jonathan Dai <nightghost5078@gmail.com>
2016-03-08 20:25:56 +0800

    Add comments.

--- app/api/1.0/user-fetch.js
+++ app/api/1.0/user-fetch.js
@@ -16,6 +16,13 @@ class UserFetch {
 
 }
 
+/**
+ * Data input:
+ * 1. url params: "/user/:id" => this.params.id
+ * 2. url query string: "/user/search?name=jonathan&age=27" => this.query.name & this.query.age
+ * 3. post body: curl -H "Content-Type: application/json" -X POST -d '{"username":"xyz","password":"xyz"}' http://localhost:3001/api/1.0/login => this.request.body.username & ...
+ */
+
 function *validate(next) {
   if (!validator.isNumeric(this.params.id)) {
     throw new Error('Invalid input: id shall be number!');
