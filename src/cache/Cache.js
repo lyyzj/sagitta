12d442f5760c86d816a0a72aabdc8568fede3214
Jonathan Dai <nightghost5078@gmail.com>
2016-04-07 20:02:22 +0800

    Cache system almost done.

--- src/cache/Cache.js
+++ src/cache/Cache.js
@@ -2,18 +2,63 @@
 
 const Redis = require('ioredis');
 
+const msgpack = require('msgpack-lite');
+
 class Cache {
 
   constructor() {
-    this.handle = null;
+    this.instance = null;
+
+    this.variance = 10;
+    this.expires = 18000; // 5 hours = 5 * 60 * 60 seconds
   }
 
   initialize(conf) {
-    this.handle = new Redis(conf);
+    this.instance = new Redis(conf);
+  }
+
+  setModelHash(modelName, shardId, queryString, data, expires) {
+    let key = this.genModelKey(modelName, shardId);
+    return this.instance.pipeline()
+      .hset(key, queryString, msgpack.encode(data))
+      .expire(key, this.genExpire(expires))
+      .exec();
+  }
+
+  getModelHash(modelName, shardId, queryString) {
+    return new Promise((resolve, reject) => {
+      this.instance.hgetBuffer(this.genModelKey(modelName, shardId), queryString).then((data) => {
+        resolve(msgpack.decode(data));
+      }).catch((err) => {
+        reject(err);
+      });
+    });
+  }
+
+  setExpire(key, expires) {
+    return this.instance.expire(key, this.genExpire(expires));
+  }
+
+  genModelKey(modelName, shardId) {
+    return `${modelName}:${shardId}`;
+  }
+
+  genExpire(expires) {
+    expires = expires || this.expires;
+
+    let varianceMin = 0;
+    let varianceMax = expires * 0.02 * this.variance;
+    let varianceMinus = expires * 0.01 * this.variance;
+
+    return Math.floor(expires + this.getRandomArbitrary(varianceMin, varianceMax) - varianceMinus);
+  }
+
+  getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
   }
 
 }
 
-const instance = new Cache();
+const cache = new Cache();
 
-module.exports = instance;
+module.exports = cache;
