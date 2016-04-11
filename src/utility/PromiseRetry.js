903ccbcbe12682e03e5871122a3c8c028055092e
Jonathan Dai <nightghost5078@gmail.com>
2016-04-11 18:57:08 +0800

    Use flat function to do utility works.

--- src/utility/PromiseRetry.js
+++ src/utility/PromiseRetry.js
@@ -1,18 +1,15 @@
 "use strict";
 
-class PromiseRetry {
+let retry = function(fn, context, args, retryLeft) {
+  retryLeft = retryLeft || 3;
+  return fn.apply(context, args).catch((err) => {
+    // logger.warn('[PromiseRetry][%s] Fn "%s" failed with %s, retry left: %d', process.pid, fn.name, err.message, retryLeft);
+    if (retryLeft <= 0) {
+      // logger.error('[PromiseRetry][%s] Fn "%s" failed with %s, no retry left', process.pid, fn.name, err.message);
+      throw err;
+    }
+    return retry(fn, context, args, retryLeft - 1);
+  });
+};
 
-  retry(fn, context, args, retryLeft = 3) {
-    return fn.apply(context, args).catch((err) => {
-      //Logger.instance.warn('[PromiseRetry][%s] Fn "%s" failed with %s, retry left: %d', process.pid, fn.name, err.message, retryLeft);
-      if (retryLeft <= 0) {
-        //Logger.instance.error('[PromiseRetry][%s] Fn "%s" failed with %s, no retry left', process.pid, fn.name, err.message);
-        throw err;
-      }
-      return this.retry(fn, context, args, retryLeft - 1);
-    });
-  }
-
-}
-
-module.exports = new PromiseRetry();
+module.exports = retry;
